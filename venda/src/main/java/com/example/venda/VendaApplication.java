3cd7f33144ae160caad4edb8fd241b2823c45e52
diegoHGp5552 <didiwtf3@gmail.com>
2024-11-27 20:51:53 -0300

    Adiciona mensagem de boas-vindas ao iniciar a aplicação

--- venda/src/main/java/com/example/venda/VendaApplication.java
+++ venda/src/main/java/com/example/venda/VendaApplication.java
@@ -8,6 +8,10 @@ public class VendaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendaApplication.class, args);
+		System.out.println("===========================================");
+		System.out.println(" Bem-vindo ao Sistema de Vendas da Rede de Lojas! 🚀 ");
+		System.out.println(" Gerencie clientes, produtos e vendas de forma eficiente! ");
+		System.out.println("===========================================");
 	}
 
 }
